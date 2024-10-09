fa926e5b8930f60b2534c9cec4d96e7a52981d79
NitinJajoria <95345434+NitinJajoria@users.noreply.github.com>
2024-10-09 18:05:08 +0530

    investors email fetch

--- routes/usersData.js
+++ routes/usersData.js
@@ -1,67 +1,69 @@
 import express from "express";
 
 import {
-  getUsersController,
-  registerUserController,
-  loginUserController,
-  getUserByIdController,
-  updateUser,
-  updateUserByIdController,
-  changePasswordController,
-  requestPasswordResetController,
-  resetPasswordController,
-  searchUsersController,
-  addEducationController,
-  addExperienceController,
-  addStartupToUserController,
-  getExploreController,
-  getExploreFiltersController,
-  addUserAsInvestorController,
-  validateSecretKeyController,
-  createSecretKeyController,
-  googleLoginController,
-  updateEducationController,
-  updateExperienceController,
-  deleteEducationController,
-  deleteExperienceController,
-  sendOTP,
-  verifyOtp,
-  createUser,
-  addInvestor,
-  addStartUp_data,
-  getLinkedInProfile,
-  handelLinkedin,
-  blockUserController,
-  getUserByIdBodyController,
-  getUsersByUserNameController,
-  unblockUserController,
+	getUsersController,
+	registerUserController,
+	loginUserController,
+	getUserByIdController,
+	updateUser,
+	updateUserByIdController,
+	changePasswordController,
+	requestPasswordResetController,
+	resetPasswordController,
+	searchUsersController,
+	addEducationController,
+	addExperienceController,
+	addStartupToUserController,
+	getExploreController,
+	getExploreFiltersController,
+	addUserAsInvestorController,
+	validateSecretKeyController,
+	createSecretKeyController,
+	googleLoginController,
+	updateEducationController,
+	updateExperienceController,
+	deleteEducationController,
+	deleteExperienceController,
+	sendOTP,
+	verifyOtp,
+	createUser,
+	addInvestor,
+	addStartUp_data,
+	getLinkedInProfile,
+	handelLinkedin,
+	blockUserController,
+	getUserByIdBodyController,
+	getUsersByUserNameController,
+	unblockUserController,
+	getUserEmailByIdController,
 } from "../controllers/userData.js";
 
-import { authenticateToken } from "../middlewares/authenticateToken.js";
+// import { authenticateToken } from "../middlewares/authenticateToken.js";
 import { update_all } from "../controllers/postController.js";
 import multer from "multer";
 const router = express.Router();
 
 const storage = multer.diskStorage({
-  destination: "./uploads",
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  },
+	destination: "./uploads",
+	filename: (req, file, cb) => {
+		cb(null, file.originalname);
+	},
 });
 
 const upload = multer({ storage });
-
-router.post("/add_user",upload.single("file"),createUser);
-router.post("/add_investor",upload.single("file"),addInvestor);
-router.post("/add_startup_data",upload.single("file"),addStartUp_data)
-router.post("/update_all", update_all)
+// http://localhost:8080/users/
+router.post("/add_user", upload.single("file"), createUser);
+router.post("/add_investor", upload.single("file"), addInvestor);
+router.post("/add_startup_data", upload.single("file"), addStartUp_data);
+router.post("/update_all", update_all);
 router.post("/login", loginUserController);
-router.post("/linkdin_login",handelLinkedin);
-router.post("/getLinkedInProfile",getLinkedInProfile)
+router.post("/linkdin_login", handelLinkedin);
+router.post("/getLinkedInProfile", getLinkedInProfile);
 router.post("/createUser", registerUserController);
-router.post("/send_otp",sendOTP);
-router.post("/verify_otp", verifyOtp)
+router.post("/send_otp", sendOTP);
+router.post("/verify_otp", verifyOtp);
 router.get("/getUserById/:id", getUserByIdController);
+router.get("/getUserEmail/:userId", getUserEmailByIdController);
 
 router.patch("/updateUserById/:userId", updateUserByIdController);
 
@@ -69,21 +71,20 @@ router.post("/requestPasswordReset", requestPasswordResetController);
 
 router.patch("/resetPassword", resetPasswordController);
 
-//validate onelink secret key 
+//validate onelink secret key
 router.post("/validateSecretKey", validateSecretKeyController);
 
 router.post("/googleLogin", googleLoginController);
 
 // Authorized routes below
-router.post("/getUserByUserName", getUsersByUserNameController)
-router.use(authenticateToken);
+router.post("/getUserByUserName", getUsersByUserNameController);
+// router.use(authenticateToken);
 
 // Profile Page
 router.patch("/updateFounder", updateUser);
 
 router.get("/getUser", getUsersController);
 router.patch("/changePassword", changePasswordController);
-
 
 router.get("/search", searchUsersController);
 
@@ -103,7 +104,7 @@ router.get("/explore", getExploreController);
 router.get("/exploreFilters", getExploreFiltersController);
 
 //block user
-router.post("/blockuser",blockUserController);
+router.post("/blockuser", blockUserController);
 //unblock user
 router.post("/unblockuser", unblockUserController);
 //getuserbyidbody
